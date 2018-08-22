30e93779204d43eb4aeee91c933af8565650b1ea
litenghui <lith@litenghuis-MacBook-Pro.local>
2018-08-22 18:01:09 +0800

    add LSTM

--- model_3_LSTM.py
+++ model_3_LSTM.py
@@ -43,9 +43,9 @@ for i in train_l:
 				y_r = pkl.load(f)
 				test_y = y_r
 for i in range(len(months[0])):
-	features.append([months[0][i],months[1][i]],months[2][i],months[3][i])
+	features.append([months[0][i],months[1][i],months[2][i],months[3][i]])
 for i in range(len(months_test[0])):
-	test_data.append([months_test[0][i],months_test[1][i]],months_test[2][i],months_test[3][i])
+	test_data.append([months_test[0][i],months_test[1][i],months_test[2][i],months_test[3][i]])
 n_test = 0
 for t_y in test_y:
 	if t_y[0] == 1:
@@ -70,24 +70,27 @@ output_w = []
 memory_w = []
 forget_w = []
 states = []
+
+
 for i in range(len(layers)-2):
 	input_w_row = []
 	output_w_row = []
 	memory_w_row = []
-	forget_w _row= []
+	forget_w_row= []
 	#这样做只存每层当前的states
 	states.append(np.zeros(layers[i+1]))
 	for j in range(month_num):
-		input_w_row.append(tf.Variable(tf.random_uniform((layers[i+1]),minval=-1,maxval=-1,dtype=tf.float32)))
-		output_w_row.append(tf.Variable(tf.random_uniform((layers[i+1]),minval=-1,maxval=-1,dtype=tf.float32)))
-		memory_w_row.append(tf.Variable(tf.random_uniform((layers[i+1]),minval=-1,maxval=-1,dtype=tf.float32)))
-		forget_w_row.append(tf.Variable(tf.random_uniform((layers[i+1]),minval=-1,maxval=-1,dtype=tf.float32)))
+		nput_w_row.append(tf.Variable(tf.zeros(layers[i+1])))
+		#input_w_row.append(tf.Variable(tf.random_uniform(layers[i+1],minval=-1,maxval=-1,dtype=tf.float32)))
+		output_w_row.append(tf.Variable(tf.random_uniform(layers[i+1],minval=-1,maxval=-1,dtype=tf.float32)))
+		memory_w_row.append(tf.Variable(tf.random_uniform(layers[i+1],minval=-1,maxval=-1,dtype=tf.float32)))
+		forget_w_row.append(tf.Variable(tf.random_uniform(layers[i+1],minval=-1,maxval=-1,dtype=tf.float32)))
 	input_w.append(input_w_row)
 	output_w.append(output_w_row)
 	memory_w.append(memory_w_row)
 	forget_w.append(forget_w_row)
 
-for j in range(month_num)
+for j in range(month_num):
 	zs = []
 	zs_plus = []
 	activates = [x[j]]
