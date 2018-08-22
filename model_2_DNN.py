890771dafd96bf8b2426b688e629d8be64ace160
litenghui <lith@litenghuis-MacBook-Pro.local>
2018-08-22 09:42:29 +0800

    change

--- model_2_DNN.py
+++ model_2_DNN.py
@@ -53,7 +53,7 @@ for i in test_l:
 			test_y = np.concatenate((test_y,y_r),axis=0)
 
 n_test = 0
-for t_y in y:
+for t_y in test_y:
 	if t_y[0] == 1:
 		n_test += 1
 
@@ -76,17 +76,17 @@ init_range = []
 
 w = [] 
 b = []
-activates = []
-zs = [x]
+activates = [x]
+zs = []
 
 for i in range(len(hidden_num)-1):
 	b_shape.append(hidden_num[i+1])
 	w_shape.append((hidden_num[i],hidden_num[i+1]))
-	init_range.append(np.sqrt(6.0/(w_shape[i][0])+w_shape[i][1]))
+	init_range.append(np.sqrt(6.0/(w_shape[i][0]+w_shape[i][1])))
 
 	b.append(tf.Variable(tf.zeros(b_shape[i],dtype=tf.float32)))
 	w.append(tf.Variable(tf.random_uniform(w_shape[i],minval=-init_range[i],maxval=init_range[i],dtype=tf.float32)))
-	zs.append(tf.matmul(zs[i],w[i]))
+	zs.append(tf.matmul(activates[i],w[i]))
 	if i != len(hidden_num)-2:
 		activates.append(tf.nn.relu(zs[-1]+b[i]))
 predict = tf.nn.softmax(zs[-1]+b[-1])
@@ -108,18 +108,18 @@ for i in range(epochs):
 		sess.run(train_step,feed_dict={x:mini_batches[j],labels:mini_batches_y[j]})
 		predict_right_tf = tf.reduce_sum(tf.cast(tf.equal(tf.argmax(predict,1),tf.argmax(labels,1)),"float"))
 		predict_right_tf_2 = tf.reduce_sum(tf.cast(tf.equal(tf.argmax(predict,1),2*tf.argmax(labels,1)),"float"))
-		predict_right = sess.run(predict_right_tf,feed_dict={x:features,labels:y})
-		predict_right_2 = sess.run(predict_right_tf_2,feed_dict={x:features,labels:y})
-		sys.stdout.write("Epochs {0}, {1}'s mini_batch:{2} / {3}, how much we predict right: {4} / {5}".format(i, j, predict_right_2, n_test, predict_right, len(y)))
+		predict_right = sess.run(predict_right_tf,feed_dict={x:test_data,labels:test_y})
+		predict_right_2 = sess.run(predict_right_tf_2,feed_dict={x:test_data,labels:test_y})
+		sys.stdout.write("Epochs {0}, {1}'s mini_batch:{2} / {3}, how much we predict right: {4} / {5}".format(i, j, predict_right_2, n_test, predict_right, len(test_y)))
 		sys.stdout.write("\r")
 		sys.stdout.flush()
 
 
 	predict_right_tf = tf.reduce_sum(tf.cast(tf.equal(tf.argmax(predict,1),tf.argmax(labels,1)),"float"))
 	predict_right_tf_2 = tf.reduce_sum(tf.cast(tf.equal(tf.argmax(predict,1),2*tf.argmax(labels,1)),"float"))
-	predict_right = sess.run(predict_right_tf,feed_dict={x:features,labels:y})
-	predict_right_2 = sess.run(predict_right_tf_2,feed_dict={x:features,labels:y})
-	print("\nEpochs {0}:{1} / {2}. how much we predict right: {3} / {4}".format(i, predict_right_2, n_test, predict_right, len(y)))
+	predict_right = sess.run(predict_right_tf,feed_dict={x:test_data,labels:test_y})
+	predict_right_2 = sess.run(predict_right_tf_2,feed_dict={x:test_data,labels:test_y})
+	print("\nEpochs {0}:{1} / {2}. how much we predict right: {3} / {4}".format(i, predict_right_2, n_test, predict_right, len(test_y)))
 
 
 
