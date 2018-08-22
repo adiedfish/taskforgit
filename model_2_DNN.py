dda288cdd9c5572cb885274c4c83792a6bb23c4d
litenghui <lith@litenghuis-MacBook-Pro.local>
2018-08-22 09:59:39 +0800

    change

--- model_2_DNN.py
+++ model_2_DNN.py
@@ -121,11 +121,11 @@ for i in range(epochs):
 	predict_right_2 = sess.run(predict_right_tf_2,feed_dict={x:test_data,labels:test_y})
 	print("\nEpochs {0}:{1} / {2}. how much we predict right: {3} / {4}".format(i, predict_right_2, n_test, predict_right, len(test_y)))
 
+	mat = [[predict_right_2,n_test-predict_right_2],[predict_right-predict_right_2, len(test_y)-n_test-predict_right+predict_right_2]]
 
-
-
-
-
+print(mat)
+with open("mat","w+") as f:
+	pkl.dump(mat,f)
 
 
 
